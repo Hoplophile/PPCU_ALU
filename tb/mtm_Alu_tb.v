e26851ff51263aa20d75b66edc608688eb89153f
Piotr Ziebinski <piotr.ziebinsk@aptiv.com>
2019-09-04 15:05:21 +0200

    send_byte and send_calc_data from tb implemention

--- tb/mtm_Alu_tb.v
+++ tb/mtm_Alu_tb.v
@@ -24,21 +24,61 @@
  * 
  * The testbench should print final PASS/FAIL text information.
  */
+ //FRAME TYPES
+ `define DATA_FRAME 1'b0
+ `define CMD_FRAME 	1'b1
 
+ //ALU OPERATIONS
+`define AND		3'b000;
+`define OR		3'b001;
+`define ADD		3'b100;
+`define SUB		3'b101;
+ 
+ 
 module mtm_Alu_tb (
     output reg clk,
     output reg rst_n,
     output reg sin,
-    input reg sout
+    input wire sout
 );
 
-	task send_byte;
-	
+	task send_byte(
+		input frame_indicator,
+		input [7:0] data
+		);
+		
+		reg [10:0] full_frame;
+		integer bit_counter;
+		
+		begin
+			full_frame = {1'b0, frame_indicator, data, 1'b1};
+			for(bit_counter = 10; bit_counter >= 0; bit_counter = bit_counter - 1) begin
+				@(negedge clk);
+				sin = full_frame[bit_counter];
+			end
+		end
 	endtask
 	
 	
-	task send_calculation_data;
-	
+	task send_calculation_data(
+		input [31:0] A, B,
+		input [2:0] OP,
+		input [3:0] CRC
+		);
+		
+		begin
+			send_byte(`DATA_FRAME, B[31:24]);
+			send_byte(`DATA_FRAME, B[23:16]);
+			send_byte(`DATA_FRAME, B[15:8]);
+			send_byte(`DATA_FRAME, B[7:0]);
+			
+			send_byte(`DATA_FRAME, B[31:24]);
+			send_byte(`DATA_FRAME, B[23:16]);
+			send_byte(`DATA_FRAME, B[15:8]);
+			send_byte(`DATA_FRAME, B[7:0]);
+			
+			send_byte(`CMD_FRAME, {1'b0, OP, CRC});
+		end
 	endtask
 	
 	
